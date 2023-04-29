1eb743e5ae0922ecd67ccc4a6b44321067ffe4fc
Ricardo Meneses Morales <ricardomeneses0503@gmail.com>
2023-04-28 20:14:42 -0600

    Comments

--- src/modules/challenge/challenge.service.ts
+++ src/modules/challenge/challenge.service.ts
@@ -2,10 +2,12 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class ChallengeService {
+  // Validación si un número es par
   isPair(number: number): boolean {
     return number % 2 === 0;
   }
 
+  // Validación si un número es primo
   isPrime(number: number): boolean {
     if (number < 2) {
       return false;
@@ -16,6 +18,7 @@ export class ChallengeService {
     return true;
   }
 
+  // Función para calcular el factorial de un número
   factorial(number: number): number {
     let factorial = 1;
     for (let i = 2; i <= number; i++) {
@@ -24,10 +27,12 @@ export class ChallengeService {
     return factorial;
   }
 
+  // Función para calcular la sumatoria de todos los números anteriores
   sumN(number: number): number {
     return (number * (number + 1)) / 2;
   }
 
+  // Función para calcular los factores de un número
   factors(number: number): number[] {
     const factors = [];
     for (let i = 1; i <= number; i++) {
@@ -36,6 +41,7 @@ export class ChallengeService {
     return factors;
   }
 
+  // Función para calcular la serie de fibonacci hasta el número dado
   fibonacci(number: number): number {
     if (number <= 1) return number;
     return this.fibonacci(number - 1) + this.fibonacci(number - 2);
